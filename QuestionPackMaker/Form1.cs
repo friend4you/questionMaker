3a27f3ff921fb0493a6043df598d7a491aef6ba2
friend4you <vlad.arsenyuk@gmail.com>
2014-09-28 22:36:27 +0400

    work part

--- QuestionPackMaker/Form1.cs
+++ QuestionPackMaker/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace QuestionPackMaker
 {
@@ -17,24 +18,74 @@ namespace QuestionPackMaker
         {
             InitializeComponent();
             Questions = new List<Question>();
-            
-            
+
+
         }
 
         private void btnAddQuestion_Click(object sender, EventArgs e)
         {
-           
-            dataGridView1.Rows[dataGridView1.RowCount-1].Cells["Question1"].Value = richTextBox1.Text;
-            
+
+            dataGridView1.Rows[dataGridView1.RowCount - 1].Cells["Question1"].Value = richTextBox1.Text;
+            dataGridView1.Rows[dataGridView1.RowCount - 1].Cells["RightAnswer"].Value = textBox1.Text;
+            dataGridView1.Rows[dataGridView1.RowCount - 1].Cells["WrongAnswer1"].Value = textBox2.Text;
+            dataGridView1.Rows[dataGridView1.RowCount - 1].Cells["WrongAnswer2"].Value = textBox3.Text;
+            dataGridView1.Rows[dataGridView1.RowCount - 1].Cells["WrongAnswer3"].Value = textBox4.Text;
+
         }
 
-        void questionsToFile(List<Question> questions)
+        public int clearCell()
         {
+            for (int i = 0; i < dataGridView1.RowCount; i++ )
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value == null) return i;
+                }
+            }
 
+                return -1;
+        }
+
+        public int fileExist()
+        {
+            for (int packNum = 0; packNum < packNum + 1; packNum++)
+            {
+                if (File.Exists("QuestionPack" + packNum + ".xml") == false) return packNum;
+            }
+            return -1;
+        }
+
+        void writeToFile(List<Question> questions)
+        {
+            int packNum = fileExist();
+            TextWriter write = new StreamWriter("QuestionPack" + packNum + ".xml");
+            write.WriteLine("<Questions>");
+            foreach (Question q in questions)
+            {
+                write.WriteLine("<Question>");
+                write.WriteLine("<Text>" + q.question + "</Text>");
+                write.WriteLine("<A>" + q.question + "</A>");
+                write.WriteLine("<B>" + q.question + "</B>");
+                write.WriteLine("<C>" + q.question + "</C>");
+                write.WriteLine("<D>" + q.question + "</D>");
+                write.WriteLine("<CorrectAnswer>" + q.question + "</CorrectAnswer>");
+                write.WriteLine("</Question>");
+                write.WriteLine("");
+                write.WriteLine("");
+            }
+            write.WriteLine("</Questions>");
+            write.Close();
         }
 
         private void btnCreatePack_Click(object sender, EventArgs e)
         {
+            int cell = clearCell();
+            if (cell != -1)
+            {
+                MessageBox.Show("Пустое значение в строке" + cell);
+                return; 
+            }
+
             for (int i = 0; i < dataGridView1.RowCount - 1; i++)
             {
                 Question question = new Question();
@@ -45,7 +96,9 @@ namespace QuestionPackMaker
                 question.wrongAnswer3 = dataGridView1.Rows[i].Cells["WrongAnswer3"].Value.ToString();
                 Questions.Add(question);
             }
-            
+            writeToFile(Questions);
+            Questions.Clear();
+            dataGridView1.Rows.Clear();
         }
     }
 }
